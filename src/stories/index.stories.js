bfe4209e7567bc66ab303b9b18f3a65ca9998f39
nguyenphuonglinh2k <nguyenphuonglinh11102000@gmail.com>
2020-06-03 16:07:20 +0700

    create stories

---
+++ src/stories/index.stories.js
@@ -0,0 +1,14 @@
+// import './BookListItem';
+import React from 'react'
+
+import NavBar from '../components/NavBar';
+import BookListItem from '../components/BookListItem';
+import LoginForm from '../components/LoginForm';
+
+export default { title: 'Default' };
+
+export const navBar = () => <NavBar />;
+
+export const bookListItem = () => <BookListItem />;
+export const loginForm = () => <LoginForm />;
+
